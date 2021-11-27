7efe1a33dc58c1ea6db28454b78e70ff762b3a99
Amir <amir.nero1@gmail.com>
2021-11-27 17:49:01 +0330

    Change api key

--- app/src/main/java/com/example/github_graphql_sample/di/NetworkModule.kt
+++ app/src/main/java/com/example/github_graphql_sample/di/NetworkModule.kt
@@ -48,7 +48,7 @@ object NetworkModule {
     fun provideHttpClient(): OkHttpClient {
         return OkHttpClient().newBuilder().addInterceptor(Interceptor { chain ->
             val request = chain.request().newBuilder().addHeader(
-                "Authorization", "bearer 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"
+                "Authorization", "bearer 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"
             )
                 .build()
             chain.proceed(request)
